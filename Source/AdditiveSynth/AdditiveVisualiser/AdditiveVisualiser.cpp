e0e4503348f1afb9ac6bde95181dd2a026176227
Jan Kalinic <jkalinic@redhat.com>
2023-02-03 12:22:23 +0100

    midi tested working
    Signed-off-by: Jan Kalinic <jkalinic@redhat.com>

--- Source/AdditiveSynth/AdditiveVisualiser/AdditiveVisualiser.cpp
+++ Source/AdditiveSynth/AdditiveVisualiser/AdditiveVisualiser.cpp
@@ -14,6 +14,7 @@ AdditiveVisualiser::AdditiveVisualiser():AudioVisualiserComponent (2)
 {
     setBufferSize(256);
     setSamplesPerBlock(25);
+    setSamplesPerBlock(25);
     setColours(C_DARK, L_GRAY);
 }
 
